48045bbda2f58c27f32f2576f4a5aa96e2231e2f
Kiran Adhikari <kiruu1238@gmail.com>
2019-06-26 21:38:15 +0545

    Added Courses Post functionalities

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,107 +1,67 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const Course = require('../../models/course');
 
-router.post('/insert', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    if (req.user.admin.is_admin === true && req.user.admin.is_techinical === true) {
-        await Team.deleteMany({})
-        const result = await Team.find({ "tournament.team.players.name": req.body.name });
-        res.json(result)
-    }
-    else {
-        res.json({
-            msg: "You should be logged in as admin to perform this task"
-        })
-    }
-})
-
-router.post('/team', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    if (req.user.admin.is_admin === true) {
-        const tournament = [], team = [], players = [];
-        players.push({
-            "name": req.body.players_name,
-            "role": req.body.players_role,
-            "image_url": req.body.players_image_url,
-            "dob": req.body.players_dob,
-            "price": req.body.players_price,
-        })
-        team.push({
-            "name": req.body.team_name,
-            "captain": req.body.team_captain,
-            "players": players
-        })
-        tournament.push({
-            "name": req.body.tournament_name,
-            "year": req.body.year,
-            "location": req.body.location,
-            team
-        })
-        const check_team = await Team.find({ 'tournament.team.name': req.body.team_name });
-        const check_tournament = await Team.find({ 'tournament.name': req.body.tournament_name, 'tournament.year': req.body.year });
-        if (!(check_tournament.length === 0)) {
-            if (!(check_team.length === 0)) {
-                const info = await Team.find({ 'tournament.team.players.name': req.body.players_name });
-                if (info === null || info.length === 0) {
-                    const rty = await Team.findOneAndUpdate({ 'tournament.team.name': req.body.team_name }, { $addToSet: { 'tournament.$.team.0.players': players } })
-                    res.json(rty)
-                }
-                else {
-                    res.json({ error: `${req.body.players_name} is already in database` })
-                }
-            } else {
-                checkduplicate(res, tournament, req.body.players_name, req.body.year, req.body.team_name)
+router.post('/add', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        if (req.user.admin.is_admin === true) {
+            const author = {
+                name: req.body.name,
+                image: req.body.image,
             }
+            const rating = {
+                star: req.body.star,
+                number: req.body.number,
+            }
+            const price = {
+                currency: req.body.currency,
+                amount: req.body.amount,
+            }
+            const course = {
+                title: req.body.title,
+                desc: req.body.desc,
+                images: req.body.images,
+                currently_enrolled: req.body.currently_enrolled,
+                author,
+                rating,
+                price
+            }
+            const obje = {
+                course
+            }
+            console.log(obje)
+            const result = await new Course(obje).save();
+            res.json(result)
         }
         else {
-            checkduplicate(res, tournament, req.body.players_name, req.body.year, req.body.team_name)
+            res.json({
+                msg: "You should be logged in as admin to perform this task"
+            })
         }
-    }
-    else {
-        res.json('You need to be logged in as Admin to perform this operation')
+    } catch (e) {
+        throw e
     }
 })
 
-router.patch('/update_players', passport.authenticate('jwt', { session: false }), async (req, res) => {
+router.get('/all/course', async (req, res) => {
+    const all = await Course.find({});
+    res.json(all)
+})
+
+router.get('/get', passport.authenticate('jwt', { session: false }), async (req, res) => {
     try {
-        const player = {
-            "name": req.body.player,
-            "role": req.body.role,
-            "image_url": req.body.image_url,
-            "dob": req.body.dob,
-            "price": req.body.price,
+        let query = {}
+        for (let key in req.query) {
+            req.query[key] !== '' ? query[key] = req.query[key] : null
         }
-        const player_to_be_updated = await Team.findOne({ 'tournament.team.players.name': req.body.player })
-        const dis = player_to_be_updated.tournament[0].team[0].players[0].id;
-        const result = await Team.updateOne({ 'tournament.team.players._id': dis }, { $set: { 'tournament.0.team.$.players': player } })
+        console.log(query)
+        const result = await Course.find(query)
         res.json(result)
     }
     catch (e) {
-        throw e
+        console.warn(e)
     }
-
 })
-
-router.get('/team', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    //const to = await ScoreBoard.insertMany(scoreboard);
-    //res.json(to);
-    const final = await Team.find({ 'tournament.team.name': req.query.team });
-    res.json(final)
-})
-
-const checkduplicate = async (res, tournament, player, year, team) => {
-    const all = new Team({
-        tournament
-    })
-    const info = await Team.find({ 'tournament.team.players.name': player, 'tournament.year': year, 'tournament.team.name': team });
-    if (info === null || info.length === 0) {
-        const sended = await all.save()
-        res.json(sended)
-    } else {
-        res.json({
-            "error": "The player is already in the database"
-        })
-    }
-
-}
 
 module.exports = router;
---
+++ models/course.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const CourseSchema = new mongoose.Schema({
+    course: {
+        title: {
+            type: String,
+            required: true
+        },
+        desc: {
+            type: String,
+            required: true
+        },
+        images: {
+            type: String,
+            required: true
+        },
+        author: {
+            name: {
+                type: String,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            }
+        },
+        rating: {
+            star: {
+                type: Number,
+                required: true
+            },
+            number: {
+                type: String,
+                required: true
+            },
+        },
+        price: {
+            currency: {
+                type: String,
+                required: true
+            },
+            amount: {
+                type: Number,
+                required: true
+            }
+        },
+        currently_enrolled: {
+            type: Number,
+            required: true
+        },
+
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = Course = mongoose.model('course', CourseSchema)
