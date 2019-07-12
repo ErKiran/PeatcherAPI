87d1433a703856dd72d64c14b31790b1477d384f
Kiran Adhikari <kiruu1238@gmail.com>
2019-07-12 21:33:08 +0545

    Validation

--- validations/user.js
+++ validations/user.js
@@ -8,6 +8,7 @@ module.exports =
             data.name = !isEmpty(data.name) ? data.name : '';
             data.email = !isEmpty(data.email) ? data.email : '';
             data.password = !isEmpty(data.password) ? data.password : '';
+            data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
             if (Validator.isEmpty(data.email)) {
                 errors.email = 'Email field is required';
@@ -15,25 +16,33 @@ module.exports =
             if (Validator.isEmpty(data.name)) {
                 errors.name = 'Name field is required';
             }
+            if (Validator.isEmpty(data.password)) {
+                errors.password = 'Password field is required';
+            }
+            if (Validator.isEmpty(data.password2)) {
+                errors.password2 = 'Password2 field is required';
+            }
             if (!Validator.isEmail(data.email)) {
                 errors.email = 'Email is invalid';
             }
             if (Validator.isEmpty(data.password)) {
                 errors.password = 'Password field is required';
             }
-
             if (!Validator.isLength(data.password, { min: 8, max: 30 })) {
                 errors.password = 'Password must be at least 8 characters and less than 30 characters';
             }
 
             if (!Validator.isLength(data.name, { min: 6, max: 30 })) {
-                errors.password = 'Name must be at least 6 characters';
+                errors.name = 'Name must be at least 6 characters';
             }
 
             if (Validator.isAlphanumeric(data.password)) {
                 errors.password = 'Password must contain atleast one special characters';
             }
 
+            if (password !== password2) {
+                errors.password2 = `Password doesn't match`
+            }
             return {
                 errors,
                 isValid: isEmpty(errors),
